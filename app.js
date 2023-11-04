97cb6ba7d7ad1c30241e7ddd181e134765a6cc66
Khari Hopes <buildingkhari@yahoo.com>
2023-11-03 22:34:50 -0500

    adding events

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ function handleSubmit(event) {
     }
   }
 
+  // function Countdown (days, hours, minutes, seconds) {
+  //   this.days = days;
+  //   this.hours = hours;
+  //   this.minutes = minutes;
+  //   this.seconds = seconds;
+  // };
+
   function countDown(){
     const today = new Date().getTime();
     const futureDate = new Date("2024-10-15T00:00:01").getTime();
@@ -35,5 +42,23 @@ function handleSubmit(event) {
   
   setInterval(countDown, 1000)
 
+  function EventDetails (name, date, time, location, description) {
+    this.name = name;
+    this.date = date;
+    this.time = time;
+    this.location = location;
+    this.description = description;
+  }
 
+  Event.prototype.getBio = function () {
+    console.log(this.name + this.location + this.time)
+  }
 
+  const greet = new Event("Meet and Greet", "2024-6-15", "12:00:00", "Appleton, Wisconsin", "social event or gathering where people come together to interact and get to know each other better. These occasions often serve as opportunities for networking, forging new connections, and establishing relationships in both personal and professional settings.");
+  greet.getBio();
+
+  const paint = new Event("Paint With Me", "2024-8-15", "20:00:00", "New York, New York", "Wine and paint nights offer a delightful combination of artistic expression and social enjoyment, as participants sip on wine while creating their own unique works of art. This relaxed and creative atmosphere encourages individuals to unwind, tap into their creativity, and connect with friends and fellow art enthusiasts.");
+  paint.getBio();
+
+  const gallery = new Event("Open Gallery", "2024-10-15", "10:00:00", "Minneapolis, Minnesota", "art exhibition or display space that is accessible to the public without any admission fees or restricted hours. These open galleries provide a platform for artists to showcase their work to a wide audience and promote artistic appreciation within the community.");
+  gallery.getBio();
